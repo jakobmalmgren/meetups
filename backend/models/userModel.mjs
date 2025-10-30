c7e3c3c2879d117496cffd0356fc4f9042b89fc5
robjor88 <robjor88@gmail.com>
2025-10-30 11:46:48 +0100

    fix name i userModel

--- backend/models/userModel.mjs
+++ backend/models/userModel.mjs
@@ -1,17 +1,28 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  name: String,
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
+const { Schema } = mongoose;
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, trim: true, maxlength: 80 },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    password: { type: String, required: true, minlength: 8, select: false },
+
+    registeredMeetups: {
+      type: [{ type: Schema.Types.ObjectId, ref: "Meetup" }],
+      default: [],
+    },
   },
-  password: { type: String, required: true, minlength: 8 },
-  registeredMeetups: [{ type: mongoose.Schema.Types.ObjectId, ref: "Meetup" }],
-});
+  { timestamps: true }
+);
+
 userSchema.index({ email: 1 }, { unique: true });
 
 export const User = mongoose.model("User", userSchema);
