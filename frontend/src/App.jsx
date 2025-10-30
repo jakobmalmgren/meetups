7b4da1b4a0f0f253f358de356f00670b66656e8b
LindbergSala <3ggjs24lindpa@folkuniversitetet.nu>
2025-10-30 10:32:17 +0100

    navbar-Update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import Navbar from "./components/navbar/Navbar.jsx";
 
 function App() {
   return (
     <>
+      <Navbar />
       <h1>hej o hå igen......</h1>
     </>
   );
