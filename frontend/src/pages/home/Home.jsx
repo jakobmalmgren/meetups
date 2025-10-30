44c334205edcf0adb9aa5e8a8267644494af26fd
LindbergSala <3ggjs24lindpa@folkuniversitetet.nu>
2025-10-30 11:04:16 +0100

    home-Update

---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,10 @@
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <h1>Welcome to the Meetups App</h1>
+      <p>Discover, join, and review meetups around you.</p>
+    </div>
+  );
+}
