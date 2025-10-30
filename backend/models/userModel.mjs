f714f1a203b8af182fad63e898b00eedcea623ae
robjor88 <robjor88@gmail.com>
2025-10-30 11:23:37 +0100

    lagt till name i userModel för att referera till ägare av rum

--- backend/models/userModel.mjs
+++ backend/models/userModel.mjs
@@ -1,19 +1,17 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema(
-  {
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-      trim: true,
-    },
-    password: { type: String, required: true, minlength: 8 },
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
   },
-  { timestamps: true }
-); // vill vi ha timestamps ? ?
-
+  password: { type: String, required: true, minlength: 8 },
+  registeredMeetups: [{ type: mongoose.Schema.Types.ObjectId, ref: Meetup }],
+});
 userSchema.index({ email: 1 }, { unique: true });
 
 export const User = mongoose.model("User", userSchema);
