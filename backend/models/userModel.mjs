7bfe0ab7cb7277d0de53de77b1e12dd3b7613317
robjor88 <robjor88@gmail.com>
2025-10-30 10:57:24 +0100

    signup route

--- backend/models/userModel.mjs
+++ backend/models/userModel.mjs
@@ -1,15 +1,18 @@
 import mongoose, { Schema, Document } from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: { type: String, required: true, minlength: 8 },
   },
-  password: { type: String, required: true, minlength: 8 },
-});
+  { timestamps: true }
+); // vill vi ha timestamps ? ?
 
 userSchema.index({ email: 1 }, { unique: true });
 
