52a252663c848d182b7d7a87a11e70c75a9be590
Alexander <aleck_johansson@hotmail.com>
2025-10-30 14:45:16 +0100

    seed mjs and seedModel

--- backend/models/meetupModel.mjs
+++ backend/models/meetupModel.mjs
@@ -1,1 +1,19 @@
 //Skapa meetupModel
+import mongoose from "mongoose";
+
+const meetupSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    host: { type: String, required: true },
+    date: { type: Date, required: true },
+    location: { type: String, required: true },
+    category: { type: String, required: true },
+    capacity: { type: Number, required: true },
+    attendees: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: "Review" }],
+  },
+  { timestaps: true }
+);
+
+export default mongoose.model("Meetup", meetupSchema);
