b17f4233018dd9a9efde0e0d76e05aa572863015
LindbergSala <3ggjs24lindpa@folkuniversitetet.nu>
2025-10-30 13:26:07 +0100

    style-Update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,7 +8,7 @@ export default function Home() {
       <div className="home-card">
         <h2>Welcome to</h2>
         <h1>Meetup <span>Z</span></h1>
-        <p>The future of meetups</p>
+        <p>-The future of meetups</p>
         <img src={meetingImage} alt="Business meeting" />
       </div>
       <Navbar />
