2671b40f617b2fab5a6c547065d77e2e7cb5a823
LindbergSala <3ggjs24lindpa@folkuniversitetet.nu>
2025-10-31 15:25:29 +0100

    icon-placement-Update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,16 @@
 import "./Home.css";
-// import SmallIcon from "../components/general-components/SmallIcon.jsx";
+import SmallIcon from "../components/general-components/SmallIcon.jsx";
 import meetingImage from "../assets/meet.png";
 import Navbar from "../components/navbar/Navbar.jsx";
 
 export default function Home() {
   return (
     <div className="home">
+      {/* Loggan i övre högra hörnet av hela sidan */}
+      <div className="icon-corner">
+        <SmallIcon />
+      </div>
+
       <div className="home-card">
         <h2>Welcome to</h2>
         <h1>
@@ -14,6 +19,7 @@ export default function Home() {
         <p>-The future of meetups</p>
         <img src={meetingImage} alt="Business meeting" />
       </div>
+
       <Navbar />
     </div>
   );
