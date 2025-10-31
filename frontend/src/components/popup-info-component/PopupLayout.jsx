e1a60ef520cc8d4318154bf456fd13e44ea33fdf
Jakob  Malmgren <jakob.malmgren1987@gmail.com>
2025-10-31 15:05:10 +0100

    gjort popup där man extra info och reviews ska ske

--- frontend/src/components/popup-info-component/PopupLayout.jsx
+++ frontend/src/components/popup-info-component/PopupLayout.jsx
@@ -1,7 +1,13 @@
 import "./PopupLayout.css";
+import PopupButtons from "./PopupButtons";
 import { FaCircleXmark } from "react-icons/fa6";
 import image from "../../assets/meet.png";
+import PopupTextArea from "./PopupTextArea";
+import { FaArrowCircleDown } from "react-icons/fa";
+import { useState } from "react";
+
 export default function PopupLayout() {
+  const [open, setOpen] = useState(false);
   return (
     <div className="popup-layout">
       <FaCircleXmark className="x"></FaCircleXmark>
@@ -9,21 +15,28 @@ export default function PopupLayout() {
         <img className="popup-layout_img" src={image} alt="" />
       </section>
       <section className="popup-layout_section-info">
-        <h1>Event: alex event</h1>
-        <p>tid & plats: Stockhiolm 24/5 kl 15.00</p>
-        <p>
-          info: Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        <h1 className="popup-layout_header">Event: alex event</h1>
+        <p className="popup-layout_time">
+          Tid & plats: Stockholm 24/5 kl 15.00
+        </p>
+        <p className="popup-layout_info">
+          Info: Lorem ipsum dolor sit amet consectetur adipisicing elit.
           Quibusdam, possimus repellendus iusto sint reiciendis reprehenderit
           esse, sequi odio voluptatum minus eum hic quaerat eius? Veritatis
           dolorum error eius sapiente dicta.
         </p>
-        <p>antal platser: 40</p>
-        <p>antal anmälda: 10</p>
+        <p className="popup-layout_amount-space">Antal platser: 40</p>
+        <p className="popup-layout_amout-signup">Antal anmälda: 10</p>
       </section>
       <section className="popup-layout_btns">
-        <button></button>
-        <button></button>
+        <PopupButtons color=" #CF8C22">Book</PopupButtons>
+        <PopupButtons onClick={() => setOpen((prev) => !prev)} color=" #CF8C22">
+          Review
+          <FaArrowCircleDown className="popup-layout_downIcon"></FaArrowCircleDown>
+        </PopupButtons>
       </section>
+
+      {open && <PopupTextArea />}
     </div>
   );
 }
