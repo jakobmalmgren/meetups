714cdd5d87060d95b74022d5d2877ea0d22aeb6a
Jakob  Malmgren <jakob.malmgren1987@gmail.com>
2025-11-03 16:41:34 +0100

    gjort login o sign up någorlunda funktionellt

--- frontend/src/api/login-signup/signup.js
+++ frontend/src/api/login-signup/signup.js
@@ -1,19 +1,21 @@
 export async function signup(email, password) {
   try {
-    const response = await fetch("", {
+    const response = await fetch("http://localhost:3000/api/users/signup", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ email, password }),
     });
-    if (!response.ok) {
-      throw new Error("network reponse was not ok");
-    }
     const data = await response.json();
+    if (!response.ok) {
+      return { success: false, error: data.error || "Unknown error" };
+    }
+
+    // console.log(data);
+
     return data;
   } catch (error) {
-    console.log("login error : ", error);
     return { success: false, error: error.message };
   }
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,8 +2,16 @@ import "./Home.css";
 import SmallIcon from "../components/general-components/SmallIcon.jsx";
 import meetingImage from "../assets/meet.png";
 import Navbar from "../components/navbar/Navbar.jsx";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    const storedUser = localStorage.getItem("userEmail");
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
   return (
     <div className="home">
       <div className="icon-corner">
@@ -15,6 +23,7 @@ export default function Home() {
         <h1>
           Meetup <span>Z</span>
         </h1>
+        <p className="home_user">{user}</p>
         <p>-The future of meetups</p>
         <img src={meetingImage} alt="Business meeting" />
       </div>
