55654a02c7bce1473406784eb8c4b810e6e2502b
Alexander <aleck_johansson@hotmail.com>
2025-11-03 13:17:55 +0100

    register and unregister function

--- backend/routes/meetupRoutes.mjs
+++ backend/routes/meetupRoutes.mjs
@@ -1,9 +1,29 @@
 import express from "express";
-import { getMeetups, getMeetupById } from "../controllers/meetupController.mjs";
+import { protect } from "../middlewares/auth.mjs";
+import { validateParams } from "../middlewares/validateParams.mjs";
+import { meetupIdSchema } from "../validators/meetupValidator.mjs";
+import {
+  getMeetups,
+  getMeetupById,
+  registerMeetup,
+  unregisterMeetup,
+} from "../controllers/meetupController.mjs";
 
 const router = express.Router();
 
 router.get("/", getMeetups);
-router.get("/:id", getMeetupById);
+router.get("/:id", validateParams(meetupIdSchema), getMeetupById);
+router.post(
+  "/:id/register",
+  protect,
+  validateParams(meetupIdSchema),
+  registerMeetup
+);
+router.delete(
+  "/:id/unregister",
+  protect,
+  validateParams(meetupIdSchema),
+  unregisterMeetup
+);
 
 export default router;
