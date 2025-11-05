30dcfce3014180322b974c00d99f2a3a17be93d0
Jakob  Malmgren <jakob.malmgren1987@gmail.com>
2025-11-05 14:00:23 +0100

    gjort reviews funktionerna

--- frontend/src/components/popup-info-component/PopupTextArea.jsx
+++ frontend/src/components/popup-info-component/PopupTextArea.jsx
@@ -2,18 +2,52 @@ import "./PopupTextArea.css";
 import { useState } from "react";
 import { FaStar } from "react-icons/fa";
 import PopupButtons from "./PopupButtons";
-// ej klar måste pilla me de lite..
+import { reviewAPi } from "../../api/reviewApi";
+// ej klar .
 
-function PopupTextArea() {
+function PopupTextArea({ meetupId }) {
   const [rating, setRating] = useState(0);
+  const [reviewText, setReviewText] = useState("");
+  const [infoText, setInfoText] = useState("");
+  const [leavingReview, setLeavingReview] = useState("");
+
   const handleClick = (value) => {
     setRating(value);
     // onRate(value);
   };
+
+  const fetchReview = async () => {
+    const data = await reviewAPi(meetupId, rating, reviewText);
+    console.log("DATA REVIEW,", data);
+    if (data.error) {
+      setInfoText(data.error);
+      setLeavingReview("error");
+      setRating(0); // nollställer stjärnorna
+      setReviewText(""); // tömmer textarea
+
+      return;
+    }
+    setInfoText("Kanon! Du har lämnat en review!");
+    setLeavingReview("success");
+    setRating(0); // nollställer stjärnorna
+    setReviewText(""); // tömmer textarea
+    setLeavingReview("");
+  };
   return (
     <section className="review">
       <h4 className="review_title">Leave a review</h4>
-      <textarea className="review_textarea" name="review" id=""></textarea>
+      <textarea
+        className="review_textarea"
+        name="review"
+        id=""
+        value={reviewText}
+        onChange={(e) => {
+          return setReviewText(e.target.value);
+        }}
+        placeholder="skriv dina recensioner här"
+      ></textarea>
+      <p className={`review ${leavingReview}`}>{infoText}</p>
+
       <div>
         {[1, 2, 3, 4, 5].map((value) => (
           <FaStar
@@ -25,7 +59,9 @@ function PopupTextArea() {
           />
         ))}
       </div>
-      <PopupButtons color="#CF8C22">Send Review</PopupButtons>
+      <PopupButtons onClick={fetchReview} color="#CF8C22">
+        Send Review
+      </PopupButtons>
     </section>
   );
 }
